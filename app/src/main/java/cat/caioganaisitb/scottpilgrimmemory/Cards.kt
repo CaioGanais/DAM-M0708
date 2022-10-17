ff3d198a3ba3693ead621671b8cb0735e1e63696
sjo <caio.ganais.7e5@itb.cat>
2022-10-17 19:55:47 +0200

    1st

---
+++ app/src/main/java/cat/caioganaisitb/scottpilgrimmemory/Cards.kt
@@ -0,0 +1,11 @@
+package cat.caioganaisitb.scottpilgrimmemory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cards : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cards)
+    }
+}
